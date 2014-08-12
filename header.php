c81fd1a68df9989c107da2d178f33424ee9aa157
robot <robot@xentralmethods.com>
2014-08-12 16:26:15 +0800

    done writer engine

--- header.php
+++ header.php
@@ -3,7 +3,16 @@
 <head>
 	<title>BROG for You</title>
 	<link href="assets/style.css" rel="stylesheet" type="text/css" />
+	<script src="assets/jquery.min.js" type="text/javascript"></script>
+	<script src="assets/markdown.js" type="text/javascript"></script>
 
+	<script>
+	$(document).ready(function(){
+      $('#md').on('keyup', function(){
+      	$('#viewer').html(markdown.toHTML($(this).val()));
+      });
+    });
+    </script>
 </head>
 <body>
 <div class="center"><div class="wrap">
@@ -16,6 +25,8 @@
 			<li><a href="#" id="generate" class="btn">Generate html</a> </li>
 			<li><a href="public" id="preview" class="btn">Preview</a> </li>
 			<li><a href="#" id="publish" class="btn">Publish</a> </li>
+			<li><a href="#" id="readme" class="btn">Readme</a> </li>
+			<li><a href="#" id="readme" class="btn">Settings</a> </li>
 			<li><a href="logout.php" id="logout" class="btn">Logout</a> </li>
 		</ul>
 	</div>
--- index.php
+++ index.php
@@ -1,2 +1,15 @@
 <?php require_once('header.php');?>
+
+
+<div id="writer">
+	<h2>Writer:</h2>
+	<div class="half">
+		<textarea id="md">#Title</textarea>
+	</div>
+	<div class="half" id="viewer">
+		
+	</div>
+</div>
+
+
 <?php require_once('footer.php');?>
